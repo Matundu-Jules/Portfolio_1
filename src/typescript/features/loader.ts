f9241e336eb47d2fa14e016598b7a6b7a4b5acd0
Matundu <matundu.pro@gmail.com>
2023-11-26 23:08:36 +0100

    Loader corrections

--- src/typescript/features/loader.ts
+++ src/typescript/features/loader.ts
@@ -1,35 +1,26 @@
 export function loader() {
     // Loader
-    document.addEventListener('DOMContentLoaded', () => {
+    window.onload = () => {
         const loader = document.querySelector('.loader-wrapper') as HTMLElement
+        const content = document.querySelector('.hidden-content') as HTMLElement
+
         if (loader) {
             loader.style.opacity = '0'
             loader.style.visibility = 'hidden'
             loader.style.transition = 'opacity 3s, visibility 3s'
-
-            setTimeout(() => {
-                loader.remove()
-                const content = document.querySelector(
-                    '.hidden-content'
-                ) as HTMLElement
-                if (content) {
-                    content.style.opacity = '1'
-                }
-            }, 3000)
-        }
-    })
-
-    // Affichez le contenu principal
-    window.onload = () => {
-        const content = document.querySelector('.hidden-content') as HTMLElement
-        if (content) {
-            // Utilisez 'fadeOut' pour une transition plus douce si vous le souhaitez
-
             content.style.display = 'block'
 
             setTimeout(() => {
-                content.style.opacity = '1'
-            }, 5000)
+                // Suppression du loader
+                loader.remove()
+
+                // Affichez le contenu principal
+                if (content) {
+                    content.style.opacity = '1'
+                    content.style.visibility = 'visible'
+                    content.style.transition = 'opacity 3s, visibility 3s'
+                }
+            }, 3000)
         }
     }
 }
