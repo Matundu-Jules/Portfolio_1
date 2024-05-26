cc8b2633a32ce94a32515b55105be6113a72f010
Matundu <matundu.pro@gmail.com>
2024-05-26 16:07:21 +0200

    Page about + responsive + correction header + maj paquets npm

--- src/features/responsive_menu.js
+++ src/features/responsive_menu.js
@@ -3,6 +3,8 @@ let isMenuEventAdded = false
 export function responsiveMenu() {
     // Gestion du menu sur le responsive
     const responsiveMenuBtn = document.querySelector('.hamburger')
+    const navElem = document.querySelector('.header-menu')
+    const navLinks = document.querySelectorAll('.header-menu a')
 
     if (!isMenuEventAdded) {
         responsiveMenuBtn.addEventListener('click', (e) => {
@@ -12,6 +14,27 @@ export function responsiveMenu() {
             const navElem = document.querySelector('nav')
             navElem.classList.toggle('open')
         })
+
+        // Fermer le menu lorsque l'on clique n'importe où sur la page
+        document.addEventListener('click', (e) => {
+            if (
+                navElem.classList.contains('open') &&
+                !navElem.contains(e.target) &&
+                !responsiveMenuBtn.contains(e.target)
+            ) {
+                navElem.classList.remove('open')
+            }
+        })
+
+        // Fermer le menu lorsque l'on clique sur un lien de navigation
+        navLinks.forEach((link) => {
+            link.addEventListener('click', () => {
+                if (navElem.classList.contains('open')) {
+                    navElem.classList.remove('open')
+                }
+            })
+        })
+
         isMenuEventAdded = true
     }
 }
