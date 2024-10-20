ed4aff0d314d9d2d30d2f425f6cc1e70c10abe88
Matundu <matundu.pro@gmail.com>
2024-10-20 11:46:28 +0200

    Redesigning the portfolio in react - initialization with vite

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,7 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    root: './src',
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, './src'),
-            '@images': resolve(__dirname, './src/assets/images'),
-        },
-    },
-    plugins: [
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'assets/images/*',
-                    dest: 'assets/images',
-                },
-                {
-                    src: 'assets/images/banner/**/*',
-                    dest: 'assets/images/banner',
-                },
-                {
-                    src: 'assets/images/exp-thumbnail/**/*',
-                    dest: 'assets/images/exp-thumbnail',
-                },
-                {
-                    src: 'assets/images/projects/**/*',
-                    dest: 'assets/images/projects',
-                },
-                {
-                    src: 'assets/documents/**/*', // Les documents sources
-                    dest: 'assets/documents', // Le répertoire de destination dans 'dist'
-                },
-            ],
-        }),
-    ],
-
-    build: {
-        outDir: '../dist',
-        emptyOutDir: true,
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, './src/index.html'),
-                about: resolve(__dirname, './src/pages/about/about.html'),
-                contact: resolve(__dirname, './src/pages/contact/contact.html'),
-                projects: resolve(
-                    __dirname,
-                    './src/pages/projects/projects.html'
-                ),
-            },
-        },
-    },
+  plugins: [react()],
 })
