03e55598734ae431dd0e4ecbe872ee833ffabfb2
Matundu jules <matundu.pro@gmail.com>
2024-12-17 11:27:23 +0100

    Refactorisation des images de projets + Refonte de la page Projets

--- src/assets/scripts/all-projects.js
+++ src/assets/scripts/all-projects.js
@@ -1,65 +1,65 @@
 export default [
     {
         title: 'RailRoad API',
-        image: '/src/assets/images/projects/railroad-api.webp',
+        image: '/src/assets/images/projects/railroad-api.png',
         category: 'Développement Web / API',
         date: '2024-10-20',
         description:
-            "Développement d'une API complète pour la gestion des trajets ferroviaires, incluant la création, modification, suppression de trains et gares, avec authentification JWT, validation des données et documentation Swagger.",
+            'API pour gérer les trajets ferroviaires avec authentification et documentation Swagger.',
         url: 'https://github.com/Matundu-Jules/railroad-api.git',
     },
     {
-        title: 'Application & Site Web <br />CLEM',
-        image: '/src/assets/images/projects/clem_logo_title_center.svg',
-        category: 'Développement Web / Application',
+        title: 'Application & Site Web CLEM',
+        image: '/src/assets/images/projects/clem.png',
+        category: 'Développement Web',
         date: '2023-07-15',
         description:
-            "CLEM était un projet innovant destiné à réinventer le partage audio, en permettant de connecter simultanément plusieurs dispositifs d'écoute sans fil.",
+            'Application innovante pour le partage audio sur plusieurs dispositifs.',
         url: '',
     },
     {
         title: 'Home Assistant',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/New_Home_Assistant_logo.svg/1200px-New_Home_Assistant_logo.svg.png',
+        image: '/src/assets/images/projects/homeassistant.png',
         category: 'IoT',
         date: '2023-06-30',
         description:
-            "Ce projet visait à maîtriser l'intégration de dispositifs connectés dans un environnement résidentiel, notamment via l'installation de Home Assistant sur Raspberry Pi pour une maison intelligente et interconnectée.",
+            'Installation de Home Assistant pour une maison connectée avec Raspberry Pi.',
         url: '',
     },
     {
         title: 'IMC-Calculator',
-        image: '/src/assets/images/projects/imc-calculator.webp',
+        image: '/src/assets/images/projects/imc-calculator.png',
         category: 'Développement Web',
         date: '2024-05-12',
         description:
-            "Ce projet est un calculateur d'Indice de Masse Corporelle (IMC) conçu pour aider les utilisateurs à évaluer leur poids corporel par rapport à leur taille.",
+            "Calculateur d'IMC pour évaluer le poids corporel par rapport à la taille.",
         url: 'https://imc-calculator.julesmatundu.com',
     },
     {
         title: 'Find in Wiki',
-        image: '/src/assets/images/projects/find-in-wiki.webp',
+        image: '/src/assets/images/projects/find-in-wiki.png',
         category: 'Développement Web',
         date: '2024-05-26',
         description:
-            "'Find in Wiki' est une application web simplifiée permettant de rechercher rapidement des sujets sur Wikipédia à l'aide d'une barre de recherche intuitive.",
+            'Application web pour rechercher rapidement des sujets sur Wikipédia.',
         url: 'https://find-in-wiki.julesmatundu.com',
     },
     {
         title: 'Cookie Creator',
-        image: '/src/assets/images/projects/cookie-creator.webp',
+        image: '/src/assets/images/projects/cookie-creator.png',
         category: 'Développement Web',
         date: '2024-05-14',
         description:
-            'CookieCreator est une application web qui permet de créer, modifier et supprimer des cookies directement depuis le navigateur.',
+            'Application pour créer, modifier et supprimer des cookies dans le navigateur.',
         url: '',
     },
     {
         title: 'TaskMaster',
-        image: '/src/assets/images/projects/taskmaster.webp',
+        image: '/src/assets/images/projects/taskMaster.png',
         category: 'Développement Web',
         date: '2024-05-16',
         description:
-            'TaskMaster est une application web qui permet aux utilisateurs de créer et de gérer des listes de tâches de manière simple et efficace.',
+            'Application pour gérer des listes de tâches facilement et efficacement.',
         url: 'https://taskmaster.julesmatundu.com/',
     },
     {
@@ -69,7 +69,7 @@ export default [
         category: 'Développement Web',
         date: '2022-03-02',
         description:
-            "Ce projet vise à intégrer une maquette de site web en HTML et CSS. <br />L'objectif principal du site est de permettre aux utilisateurs de trouver des hébergements et des activités dans la ville de leur choix.",
+            'Site web permettant de trouver des hébergements et activités par ville.',
         url: 'https://booki.julesmatundu.com',
     },
     {
@@ -79,17 +79,17 @@ export default [
         category: 'Développement Web',
         date: '2022-03-18',
         description:
-            "Ce projet vise à créer un site web 'mobile first' pour une startup de restauration. <br />Le site permet de répertorier les menus de restaurants gastronomiques et offre la possibilité aux clients de composer leur repas à l'avance.",
+            "Site mobile-first pour réserver des menus de restaurants à l'avance.",
         url: 'https://ohmyfood.julesmatundu.com',
     },
     {
         title: 'La Panthère',
         subtitle: 'OpenClassrooms P4',
-        image: '/src/assets/images/projects/la-panthere.webp',
+        image: '/src/assets/images/projects/lapanthere.png',
         category: 'Développement Web',
         date: '2022-04-18',
         description:
-            "Le but de ce projet est d'améliorer le référencement du site web de l'agence de web design La Panthère, basée à Lyon, afin de renforcer sa visibilité sur les moteurs de recherche et de résoudre les problèmes d'accessibilité.",
+            "Amélioration du référencement et de l'accessibilité du site La Panthère.",
         url: 'https://la-panthere.julesmatundu.com',
     },
     {
@@ -99,7 +99,7 @@ export default [
         category: 'Développement Web',
         date: '2022-05-25',
         description:
-            'Ce projet vise à créer une plateforme de e-commerce pour la marque de canapés Kanap, permettant de vendre leurs produits en ligne en plus de leur boutique physique. <br />Le site comprend une partie front-end en JavaScript pur et une API back-end.',
+            'Site e-commerce pour la vente de canapés avec front-end en JavaScript.',
         url: 'https://github.com/Matundu-Jules/OpenClassrooms-P5-Kanap.git',
     },
     {
@@ -109,7 +109,7 @@ export default [
         category: 'Développement Web',
         date: '2022-07-06',
         description:
-            "Ce projet vise à créer une API pour une application web de critique des sauces piquantes appelée 'Hot Takes'. <br />L'application permet aux utilisateurs de télécharger leurs sauces piquantes préférées et de liker ou disliker les sauces partagées par d'autres utilisateurs.",
+            'API pour une application web de critique de sauces piquantes.',
         url: 'https://github.com/Matundu-Jules/OpenClassrooms-P6-Piiquante-API.git',
     },
     {
@@ -119,7 +119,7 @@ export default [
         category: 'Développement Web',
         date: '2022-08-20',
         description:
-            "Ce projet vise à créer un réseau social interne pour les employés de Groupomania, une grande entreprise spécialisée dans la grande distribution. <br />L'objectif est de faciliter les interactions entre collègues grâce à une plateforme conviviale et fonctionnelle.",
+            'Réseau social interne pour faciliter les interactions entre employés.',
         url: 'https://github.com/Matundu-Jules/OpenClassrooms-P7-Groupomania.git',
     },
 ]
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,3 +1,94 @@
+// import styles from './Projects.module.scss'
+// import projects from '../../assets/scripts/all-projects.js'
+
+// function Projects() {
+//     // Fonction de tri des projets
+//     const sortedProjects = projects.sort((a, b) => {
+//         const dateA = a.date || ''
+//         const dateB = b.date || ''
+//         const dateCompare = dateB.localeCompare(dateA)
+
+//         if (dateCompare !== 0) return dateCompare
+
+//         const aCriteria = a.subtitle || a.title
+//         const bCriteria = b.subtitle || b.title
+//         return aCriteria.localeCompare(bCriteria)
+//     })
+
+//     return (
+//         <main className={styles['projects-container']}>
+//             <h1 className={styles['project-main-title']}>Mes projets</h1>
+//             <section className={styles['project-gallery']} id="project-gallery">
+//                 {sortedProjects.map((project, index) => (
+//                     <div key={index} className={styles['project-card']}>
+//                         <div className={styles['project-card-content']}>
+//                             {/* Image du projet */}
+//                             <div
+//                                 className={styles['project-card-img-container']}
+//                             >
+//                                 <img
+//                                     src={project.image}
+//                                     alt={`Logo de ${project.title}`}
+//                                     className={styles['project-card-img']}
+//                                 />
+//                             </div>
+
+//                             {/* Détails du projet */}
+//                             <div className={styles['project-card-body']}>
+//                                 <div className={styles['project-card-details']}>
+//                                     <p
+//                                         className={
+//                                             styles['project-card-domain']
+//                                         }
+//                                     >
+//                                         {project.category}
+//                                     </p>
+//                                     <p className={styles['project-card-date']}>
+//                                         {project.date}
+//                                     </p>
+//                                 </div>
+//                                 <h2 className={styles['project-card-title']}>
+//                                     {project.title}
+//                                 </h2>
+//                                 {project.subtitle && (
+//                                     <h3
+//                                         className={
+//                                             styles['project-card-subtitle']
+//                                         }
+//                                     >
+//                                         {project.subtitle}
+//                                     </h3>
+//                                 )}
+//                                 <p
+//                                     className={
+//                                         styles['project-card-description']
+//                                     }
+//                                 >
+//                                     {project.description}
+//                                 </p>
+//                                 {project.url && (
+//                                     <a
+//                                         href={project.url}
+//                                         target="_blank"
+//                                         rel="noopener noreferrer"
+//                                         className={styles['project-card-link']}
+//                                     >
+//                                         Voir le projet
+//                                     </a>
+//                                 )}
+//                             </div>
+//                         </div>
+//                     </div>
+//                 ))}
+//             </section>
+//         </main>
+//     )
+// }
+
+// export default Projects
+
+import { format } from 'date-fns'
+import { fr } from 'date-fns/locale'
 import styles from './Projects.module.scss'
 import projects from '../../assets/scripts/all-projects.js'
 
@@ -16,62 +107,91 @@ function Projects() {
     })
 
     return (
-        <main className={styles['projects-container']}>
-            <h1 className={styles['project-main-title']}>Mes projets</h1>
-            <section className={styles['project-gallery']} id="project-gallery">
-                {sortedProjects.map((project, index) => (
-                    <div key={index} className={styles['project-card']}>
-                        <div className={styles['project-card-content']}>
-                            {/* Image du projet */}
-                            <div
-                                className={styles['project-card-img-container']}
-                            >
-                                <img
-                                    src={project.image}
-                                    alt={`Logo de ${project.title}`}
-                                    className={styles['project-card-img']}
-                                />
+        <section id={styles.projects}>
+            <h2 id="projects-title">Mes Projets</h2>
+            <div className={styles.cardsContainer}>
+                {sortedProjects.map((project) => (
+                    <div key={project.title} className={styles.card}>
+                        <div className={styles.cardInner}>
+                            {/* Face avant */}
+                            <div className={styles.cardFront}>
+                                <div
+                                    className={
+                                        styles[
+                                            'project-home-card-img-container'
+                                        ]
+                                    }
+                                >
+                                    <img
+                                        src={project.image}
+                                        alt={`Logo de ${project.title}`}
+                                        className={
+                                            styles['project-home-card-img']
+                                        }
+                                    />
+                                </div>
                             </div>
+                            {/* Face arrière */}
+                            <div className={styles.cardBack}>
+                                <h3
+                                    className={
+                                        styles['project-home-card-title']
+                                    }
+                                >
+                                    {project.title}
+                                </h3>
+                                {project.subtitle && (
+                                    <h4
+                                        className={
+                                            styles['project-home-card-subtitle']
+                                        }
+                                    >
+                                        {project.subtitle}
+                                    </h4>
+                                )}
 
-                            {/* Détails du projet */}
-                            <div className={styles['project-card-body']}>
-                                <div className={styles['project-card-details']}>
+                                <div
+                                    className={
+                                        styles['project-home-card-details']
+                                    }
+                                >
                                     <p
                                         className={
-                                            styles['project-card-domain']
+                                            styles['project-home-card-domain']
                                         }
                                     >
                                         {project.category}
                                     </p>
-                                    <p className={styles['project-card-date']}>
-                                        {project.date}
-                                    </p>
-                                </div>
-                                <h2 className={styles['project-card-title']}>
-                                    {project.title}
-                                </h2>
-                                {project.subtitle && (
-                                    <h3
+
+                                    <p
                                         className={
-                                            styles['project-card-subtitle']
+                                            styles['project-home-card-date']
                                         }
                                     >
-                                        {project.subtitle}
-                                    </h3>
-                                )}
+                                        {format(
+                                            new Date(project.date),
+                                            'd MMM yyyy',
+                                            { locale: fr }
+                                        )}
+                                    </p>
+                                </div>
+
                                 <p
                                     className={
-                                        styles['project-card-description']
+                                        styles['project-home-card-description']
                                     }
                                 >
                                     {project.description}
                                 </p>
+
                                 {project.url && (
                                     <a
                                         href={project.url}
+                                        className={
+                                            styles['project-home-card-link']
+                                        }
                                         target="_blank"
                                         rel="noopener noreferrer"
-                                        className={styles['project-card-link']}
                                     >
                                         Voir le projet
                                     </a>
@@ -80,8 +200,8 @@ function Projects() {
                         </div>
                     </div>
                 ))}
-            </section>
-        </main>
+            </div>
+        </section>
     )
 }
 
